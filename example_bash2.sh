e47bcd3d27cafda06828af6277559b4f63d8f86b
mopuchlik <michal.opuchlik@gmail.com>
2023-07-25 17:28:39 +0200

    another simple script

--- example_bash2.sh
+++ example_bash2.sh
@@ -7,13 +7,13 @@ echo -n 'podaj liczbe 2: '
 read l2
 
 suma=$(($l1+$l2))
-echo -n "$suma"
+echo "$suma"
 
 # less than or equal
 # https://tldp.org/LDP/abs/html/comparison-ops.html
 if [ $suma -le 10 ]
 then
-       echo -n "suma wynosi:  $suma"
+       echo "suma wynosi:  $suma"
 else
-       echo -n "suma jest > 10 (wynosi $suma) wiec nie ma printu"
+       echo "suma jest > 10 (wynosi $suma) wiec nie ma printu"
 fi
